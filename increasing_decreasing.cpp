c0f58fb2dcc956b050b0c2f9cda5c1c918b61afd
lakshmank6300 <119840360+lakshmank6300@users.noreply.github.com>
2023-10-15 12:16:21 +0530

    Add files via upload

---
+++ increasing_decreasing.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+void inc_dec(int n){
+    if(n==0)
+    return;
+    cout<<n<<" ";
+    inc_dec(n-1);
+    cout<<n<<" ";
+}
+int main(){
+    int n;
+    cin>>n;
+    inc_dec(n);
+}
